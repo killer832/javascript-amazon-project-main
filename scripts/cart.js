b33731bdbc48cac3d7bb70526adc59932892b4d8
Tort <mrvkiller4@gmail.com>
2026-01-23 14:31:05 +0530

    implement localstorage

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,14 +1,21 @@
 import {products,hello} from  '../data/products.js'
 
-
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity:2
-  },
-  {
-  productId:  "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity:1
-  }];
+  function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart))
+  }
+  
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if (!cart){
+  cart =
+  [{
+   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+   quantity:2
+   },
+   {
+   productId:  "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+   quantity:1
+   }];
+}
 export function addTocart(productId,timer){
     let matchItem;
     let cartQuantity =0;
@@ -44,8 +51,7 @@ export function addTocart(productId,timer){
         added.classList.remove('js-added-message')
       },2000)
     },50);
-  
-  
+    saveToStorage() 
 }
  export function removeFromCart(productId){
   let newCart =[];
@@ -60,4 +66,5 @@ export function addTocart(productId,timer){
     
   
   cart = newCart
+  saveToStorage()
 }
